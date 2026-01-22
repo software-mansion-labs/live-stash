c123e78fbbbb103cc7e6b7accddca15559e62d2e
kraleppa <krzysztof.nalepa@swmansion.com>
2026-01-22 17:59:51 +0100

    Add basic storage

--- lib/live_stash/application.ex
+++ lib/live_stash/application.ex
@@ -3,6 +3,6 @@ defmodule LiveStash.Application do
   use Application
 
   def start(_type, _args) do
-    Supervisor.start_link([], strategy: :one_for_one)
+    Supervisor.start_link([LiveStash.Server.Storage], strategy: :one_for_one)
   end
 end
---
+++ lib/live_stash/server/storage.ex
@@ -0,0 +1,90 @@
+defmodule LiveStash.Server.Storage do
+  @moduledoc false
+
+  use GenServer
+
+  @table_name :live_stash_server_storage
+
+  def start_link(opts \\ []) do
+    GenServer.start_link(__MODULE__, opts, name: __MODULE__)
+  end
+
+  @impl true
+  def init(_opts) do
+    :ets.new(@table_name, [
+      :set,
+      :public,
+      :named_table,
+      {:write_concurrency, true},
+      {:decentralized_counters, true}
+    ])
+
+    {:ok, %{}}
+  end
+
+  @spec insert_state(id :: term(), state :: map()) :: :ok | {:error, any()}
+  def insert_state(id, state) do
+    :ets.insert(@table_name, {id, self(), System.os_time(), state})
+    :ok
+  rescue
+    error ->
+      {:error, error}
+  end
+
+  @spec update_state(id :: term(), key :: term(), value :: term()) :: :ok | {:error, any()}
+  def update_state(id, key, value) do
+    @table_name
+    |> :ets.lookup(id)
+    |> case do
+      [{^id, _pid, _time, map_state}] ->
+        new_map = Map.put(map_state, key, value)
+        insert_state(id, new_map)
+
+      [] ->
+        {:error, :not_found}
+    end
+  rescue
+    error ->
+      {:error, error}
+  end
+
+  @spec change_owner(id :: term(), new_owner :: pid()) :: :ok | {:error, any()}
+  def change_owner(id, new_owner) do
+    :ets.update_element(@table_name, id, [{2, new_owner}])
+    :ok
+  rescue
+    error ->
+      {:error, error}
+  end
+
+  @spec bump_timestamp(id :: term()) :: :ok | {:error, any()}
+  def bump_timestamp(id) do
+    :ets.update_element(@table_name, id, [{3, System.os_time()}])
+    :ok
+  rescue
+    error ->
+      {:error, error}
+  end
+
+  @spec delete_state(id :: term()) :: :ok | {:error, any()}
+  def delete_state(id) do
+    :ets.delete(@table_name, id)
+    :ok
+  rescue
+    error ->
+      {:error, error}
+  end
+
+  @spec get_state(id :: term()) :: {:ok, map()} | {:error, any()}
+  def get_state(id) do
+    @table_name
+    |> :ets.lookup(id)
+    |> case do
+      [{^id, _pid, _time, state}] -> {:ok, state}
+      [] -> {:error, :not_found}
+    end
+  rescue
+    error ->
+      {:error, error}
+  end
+end
