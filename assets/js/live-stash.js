b0f5d3611bc6a441b107876bf34a35c9028072c6
Filip Wegrzyn <filipwegrzyn85@gmail.com>
2026-03-06 17:17:22 +0100

    Fix client mode state reset bug

--- assets/js/live-stash.js
+++ assets/js/live-stash.js
@@ -4,6 +4,14 @@ window.addEventListener("phx:live-stash:reset", (_event) => {
   stashedState = {}
 })
 
+window.addEventListener("phx:page-loading-start", (info) => {
+
+
+  if (info.detail && info.detail.kind === "redirect" || info.detail.kind === "patch") {
+    stashedState = {};
+  }
+});
+
 window.addEventListener("phx:live-stash:stash", (event) => {
   stashedState[event.detail.key] = event.detail.value;
 })
