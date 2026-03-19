70beb7ca03af2aefde9bff691c4fa9cffc7e875f
Filip Wegrzyn <filipwegrzyn85@gmail.com>
2026-03-19 09:35:52 +0100

    Add NodeHint test

---
+++ test/live_stash/server/node_hint_test.exs
@@ -0,0 +1,92 @@
+defmodule LiveStash.Server.NodeHintTest do
+  use ExUnit.Case, async: true
+  import ExUnit.CaptureLog
+
+  alias LiveStash.Server.NodeHint
+  alias Phoenix.LiveView.Socket
+
+  defmodule MockEndpoint do
+    def config(:secret_key_base) do
+      String.duplicate("abcdefghijklmnopqrstuvwxyz012345", 2)
+    end
+  end
+
+  setup do
+    secret = "my_test_secret"
+
+    socket = %Socket{
+      endpoint: MockEndpoint,
+      private: %{live_stash: %{secret: secret}}
+    }
+
+    {:ok, socket: socket, secret: secret}
+  end
+
+  describe "create_node_hint/1" do
+    test "encrypts the current node using the socket's secret", %{socket: socket, secret: secret} do
+      hint = NodeHint.create_node_hint(socket)
+
+      assert is_binary(hint)
+
+      assert {:ok, decrypted_node} = Phoenix.Token.decrypt(socket, secret, hint)
+      assert decrypted_node == :erlang.atom_to_binary(Node.self())
+    end
+  end
+
+  describe "get_node_hint/3" do
+    test "decrypts a valid node hint and returns the node as an atom", %{
+      socket: socket,
+      secret: secret
+    } do
+      node_binary = :erlang.atom_to_binary(Node.self())
+      valid_hint = Phoenix.Token.encrypt(socket, secret, node_binary)
+
+      params = %{"node" => valid_hint}
+
+      assert NodeHint.get_node_hint(socket, params, secret) == Node.self()
+    end
+
+    test "returns nil when connect_params is nil", %{socket: socket, secret: secret} do
+      assert NodeHint.get_node_hint(socket, nil, secret) == nil
+    end
+
+    test "returns nil when the 'node' key is missing from connect_params", %{
+      socket: socket,
+      secret: secret
+    } do
+      params = %{"some_other_key" => "value"}
+      assert NodeHint.get_node_hint(socket, params, secret) == nil
+    end
+
+    test "returns nil and logs a warning when decryption fails (invalid token)", %{
+      socket: socket,
+      secret: secret
+    } do
+      params = %{"node" => "invalid_or_tampered_token"}
+
+      log =
+        capture_log(fn ->
+          assert NodeHint.get_node_hint(socket, params, secret) == nil
+        end)
+
+      assert log =~ "Failed to decode node hint"
+    end
+
+    test "returns nil and logs a warning when decrypted node is not an existing atom", %{
+      socket: socket,
+      secret: secret
+    } do
+      non_existent_atom_string = "non_existent_node_xyz_123456789"
+      hint = Phoenix.Token.encrypt(socket, secret, non_existent_atom_string)
+
+      params = %{"node" => hint}
+
+      log =
+        capture_log(fn ->
+          assert NodeHint.get_node_hint(socket, params, secret) == nil
+        end)
+
+      assert log =~ "Failed to decode node hint"
+    end
+  end
+end
