35cda444bae554001883ffdc6ccd9db004dcb69f
Filip Wegrzyn <filipwegrzyn85@gmail.com>
2026-04-20 17:54:24 +0200

    test

---
+++ test/live_stash/live_stash_test.exs
@@ -0,0 +1,32 @@
+defmodule LiveStashTest do
+  use ExUnit.Case, async: true
+
+  alias LiveStash.Fakes
+
+  setup do
+    socket =
+      Fakes.socket(
+        assigns: %{},
+        private: %{
+          live_temp: %{},
+          connect_params: %{},
+          lifecycle: %Phoenix.LiveView.Lifecycle{}
+        }
+      )
+
+    {:ok, socket: socket}
+  end
+
+  test "init_stash/3 attaches :after_render hook when auto_stash is true (default)", %{
+    socket: socket
+  } do
+    socket_with_hook = LiveStash.init_stash(socket, %{}, stored_keys: [:count])
+
+    assert %{after_render: hooks} = socket_with_hook.private.lifecycle
+
+    assert Enum.any?(hooks, fn
+             %{id: id} -> id == :live_stash_auto_stash
+             {id, _function} -> id == :live_stash_auto_stash
+           end)
+  end
+end
