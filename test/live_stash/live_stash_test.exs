b80cbc4bb5d5ed808f2d70909459a5b8e6183caf
Filip Wegrzyn <filipwegrzyn85@gmail.com>
2026-04-21 12:34:33 +0200

    tests

--- test/live_stash/live_stash_test.exs
+++ test/live_stash/live_stash_test.exs
@@ -17,10 +17,36 @@ defmodule LiveStashTest do
     {:ok, socket: socket}
   end
 
-  test "init_stash/3 attaches :after_render hook when auto_stash is true (default)", %{
+  test "init_stash/3 attaches after_render hook when auto_stash is true by default", %{
     socket: socket
   } do
     socket_with_hook = LiveStash.init_stash(socket, %{}, stored_keys: [:count])
+
+    assert %{after_render: hooks} = socket_with_hook.private.lifecycle
+
+    assert Enum.any?(hooks, fn
+             %{id: id} -> id == :live_stash_auto_stash
+             {id, _function} -> id == :live_stash_auto_stash
+           end)
+  end
+
+  test "init_stash/3 does not attach after_render hook when auto_stash is false", %{
+    socket: socket
+  } do
+    socket_without_hook =
+      LiveStash.init_stash(socket, %{}, stored_keys: [:count], auto_stash: false)
+
+    assert %{after_render: hooks} = socket_without_hook.private.lifecycle
+
+    refute Enum.any?(hooks, fn
+             %{id: id} -> id == :live_stash_auto_stash
+             {id, _function} -> id == :live_stash_auto_stash
+           end)
+  end
+
+  test "init_stash/3 accepts explicit auto_stash true", %{socket: socket} do
+    socket_with_hook =
+      LiveStash.init_stash(socket, %{}, stored_keys: [:count], auto_stash: true)
 
     assert %{after_render: hooks} = socket_with_hook.private.lifecycle
 
